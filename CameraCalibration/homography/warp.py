7e45150501bc7c8853f469019b8a46a27f03abe0
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-02-02 01:27:09 -0500

    Work with warping image

--- CameraCalibration/homography/warp.py
+++ CameraCalibration/homography/warp.py
@@ -5,7 +5,8 @@ import matplotlib as plt
 src_img = cv2.imread('/Users/Benjamin/PycharmProjects/SeniorProject/LaneDetection/Samples/bluelane.jpg')
 cv2.imshow('src_img',src_img)
 
-src_pts = np.float32([[190,75],[470,70],[-10,380],[560,400]]) #src
+src_pts = np.float32([[343,386],[857,386],[0,558],[1256,585]]) #src
+
 dst_pts = np.float32([[0,0],[640,0],[0,426],[640,426]]) #dst
 
 M = cv2.getPerspectiveTransform(src_pts,dst_pts)
