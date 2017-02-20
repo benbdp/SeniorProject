dbb656a4be2fa45e72954bc4f36cb1187c1723a5
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-02-20 15:41:40 -0500

    work on raspberry pi

--- Raspberry Pi/SerialTest.py
+++ Raspberry Pi/SerialTest.py
@@ -63,31 +63,16 @@ def ultrasonicright():
 
 
 while True:
-    time.sleep(0.1) # sampling rate
-
-
+    time.sleep(0.01) # sampling rate
     left_distance = ultrasonicleft()
     right_distance = ultrasonicright()
-
-    #
-    #
-    #
-    #
-    #
-
-
-    #Outputs:
-    #right_distance
-    #left_distance
-    #right_distance = int(input('enter right_distance: '))
-    #left_distance = int(input('enter left_distance: '))
-
-
     if (right_distance > distance_limit) and (left_distance > distance_limit):
-        motor_speed = str(100)
-        ser.write(motor_speed+"m,")
-        #print(motor_speed+"m,")
+        motor_speed = (bytes(100, 'UTF-8'))
+        ser.write(motor_speed+(bytes("m", 'UTF-8')))
+        print(motor_speed+"m,")
+        print(motor_speed + (bytes("m", 'UTF-8')))
     else:
-        motor_speed=str(0)
-        ser.write(motor_speed+"m,")
-        #print(motor_speed+"m,")
+        motor_speed = (bytes(0, 'UTF-8'))
+        ser.write(motor_speed+(bytes("m", 'UTF-8')))
+        print(motor_speed+"m,")
+        print(motor_speed+(bytes("m", 'UTF-8')))
