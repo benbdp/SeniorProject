f6e6aaa678e45dd328f87109bb58b5275bea12e9
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-02-24 10:56:01 -0500

    camera calibration for raspberry pi camera

--- RaspberryPi/SerialTest.py
+++ RaspberryPi/SerialTest.py
@@ -67,12 +67,12 @@ try:
         right_distance = ultrasonicright()
         print(right_distance)
         if (right_distance > distance_limit) and (left_distance > distance_limit):
-            motor_speed = ord(100)
+            motor_speed = ord(str(100))
             #ser.write(motor_speed+(bytes("m", 'ascii')))
             #print(motor_speed+"m,")
-            print(motor_speed + ord(m))
+            print(motor_speed + ord('m'))
         else:
-            motor_speed = ord(100)
+            motor_speed = ord(str(100))
             #ser.write(motor_speed+(bytes("m", 'ascii')))
             #print(motor_speed+"m,")
             print(motor_speed+ord('m'))
