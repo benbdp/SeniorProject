7e9ea5e0381db0ed21db8488903971d3fff7c6a6
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-02-24 13:38:15 -0500

    serial debugging

--- RaspberryPi/SerialTest.py
+++ RaspberryPi/SerialTest.py
@@ -70,13 +70,12 @@ try:
             motor_speed = str(100)
             msg = motor_speed + str('m')
             print([ord(c) for c in msg])
-            #print(motor_speed+"m,")
-            #print(motor_speed + ord('m'))
+            ser.write([ord(c) for c in msg])
         else:
             motor_speed = str(0)
             msg = motor_speed + str('m')
             print([ord(c) for c in msg])
-            #print(motor_speed+"m,")
+            ser.write([ord(c) for c in msg])
 except KeyboardInterrupt:
     print("Usser Stopped")
     ser.close()
