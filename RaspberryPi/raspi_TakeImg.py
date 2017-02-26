48478202292fc706b5f5bb38734c0d9d457de0b1
Benjamin <benjamin.plamondon@gmail.com>
2017-02-26 17:26:02 -0500

    taking images on pi

--- RaspberryPi/raspi_TakeImg.py
+++ RaspberryPi/raspi_TakeImg.py
@@ -17,6 +17,6 @@ camera.capture(rawCapture, format="bgr")
 image = rawCapture.array
 # display the image on screen and wait for a keypress
 
-cv2.imshow('img',image)
-cv2.waitKey()
-cv2.destroyAllWindows()
+while True:
+    cv2.imshow('img',image)
+    cv2.waitKey(10)
