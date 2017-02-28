d60b32d3742a9c4834ed78da085a5d24477c5070
Benjamin <benjamin.plamondon@gmail.com>
2017-02-28 16:25:43 -0500

    taking images on pi

--- RaspberryPi/raspi_TakeImg.py
+++ RaspberryPi/raspi_TakeImg.py
@@ -1,22 +1,31 @@
+# import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-import cv2
 import time
+import cv2
 
+# initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
+camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
-num = 0
-maxFrames = 15
 
 # allow the camera to warmup
 time.sleep(0.1)
 
-# grab an image from the camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
-# display the image on screen and wait for a keypress
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
 
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
 
-cv2.imshow('img',image)
-cv2.waitKey()
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
