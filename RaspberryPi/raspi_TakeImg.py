a5877c835c46837c32a0610be814cd2ff873c8e2
Benjamin <benjamin.plamondon@gmail.com>
2017-02-28 16:36:57 -0500

    taking images on pi

--- RaspberryPi/raspi_TakeImg.py
+++ RaspberryPi/raspi_TakeImg.py
@@ -21,11 +21,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     # show the frame
     cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
+    cv2.waitkey()
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
-
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
