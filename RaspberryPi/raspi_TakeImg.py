b55add83ad12eb4e8a235a7a82b2a1f2737a18f6
Benjamin <benjamin.plamondon@gmail.com>
2017-02-28 16:42:48 -0500

    taking images on pi

--- RaspberryPi/raspi_TakeImg.py
+++ RaspberryPi/raspi_TakeImg.py
@@ -9,6 +9,7 @@ camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
+num = 1
 
 # allow the camera to warmup
 time.sleep(0.1)
@@ -22,6 +23,12 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # show the frame
     cv2.imshow("Frame", image)
     cv2.waitKey()
+
+    entry = int(input("Enter 1 to save: "))
+
+    if (entry == int(1)):
+        cv2.imwrite("/home/pi/Cal_Imgsimage%04i.jpg" % num, image)
+        num = num + 1
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
