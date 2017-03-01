2672d4442d8b53c5179674bd0724dc2a6689c01b
Benjamin <benjamin.plamondon@gmail.com>
2017-03-01 11:35:45 -0500

    drive testing with ultrasonic

--- RaspberryPi/raspi_Drive.py
+++ RaspberryPi/raspi_Drive.py
@@ -92,21 +92,6 @@ try:
             ser.write (motor_speed + str('m,') + servo_angle + str('s,'))
 
 
-            # Camera stuff
-            for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-                # grab the raw NumPy array representing the image, then initialize the timestamp
-                # and occupied/unoccupied text
-                image = frame.array
-
-                # show the frame
-                cv2.imshow("Frame", image)
-                key = cv2.waitKey(1) & 0xFF
-
-                # clear the stream in preparation for the next frame
-                rawCapture.truncate(0)
-
-                # if the `q` key was pressed, break from the loop
-
 
             #if turn_angle < 0:
 
