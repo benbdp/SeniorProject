706ebf3ac89287b4c70d46e212187d6f8d4b3b26
Benjamin <benjamin.plamondon@gmail.com>
2017-03-01 13:40:53 -0500

    copy test code from Cal.py

--- RaspberryPi/raspi_Drive.py
+++ RaspberryPi/raspi_Drive.py
@@ -72,7 +72,7 @@ with picamera.PiCamera() as camera:
         with picamera.array.PiRGBArray(camera) as stream:
             camera.capture(stream, 'bgr', use_video_port=True)
             hsv = cv2.cvtColor(stream.array, cv2.COLOR_BGR2HSV)
-            #cv2.imshow('frame', hsv)
+            cv2.imshow('frame', hsv)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
