b35363b43c26abe2290449ad4929ab323caee93b
Benjamin <benjamin.plamondon@gmail.com>
2017-03-01 13:19:57 -0500

    undistort and camera calibration

--- RaspberryPi/Cam.py
+++ RaspberryPi/Cam.py
@@ -13,6 +13,8 @@ with picamera.PiCamera() as camera:
         with picamera.array.PiRGBArray(camera) as stream:
             camera.capture(stream, 'bgr', use_video_port=True)
             hsv = cv2.cvtColor(stream.array, cv2.COLOR_BGR2HSV)
+            cv2.imshow('frame', hsv)
+            cv2.waitKey(1)
             print 'Average H: %.2f, S: %.2f, V: %.2f' % (
                 np.average(hsv[..., 0]),
                 np.average(hsv[..., 1]),
