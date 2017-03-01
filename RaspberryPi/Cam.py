29bad55281fba7d737593e21fc9e662b8823fc61
Benjamin <benjamin.plamondon@gmail.com>
2017-03-01 13:28:12 -0500

    undistort and camera calibration

--- RaspberryPi/Cam.py
+++ RaspberryPi/Cam.py
@@ -4,6 +4,61 @@ import time
 import cv2
 import numpy as np
 
+distance_limit = 25
+GPIO.setmode(GPIO.BCM)
+TRIGGERRIGHT = 18
+ECHORIGHT = 23
+GPIO.setup(TRIGGERRIGHT, GPIO.OUT)
+GPIO.setup(ECHORIGHT, GPIO.IN)
+TRIGGERLEFT = 24
+ECHOLEFT = 25
+GPIO.setup(TRIGGERLEFT, GPIO.OUT)
+GPIO.setup(ECHOLEFT, GPIO.IN)
+
+mtx = np.matrix([[486.7350296, 0., 319.86577798],
+ [0., 485.14619589, 242.13957787],
+ [0., 0., 1.]])
+#print mtx
+dist = np.matrix([[1.72030848e-01,  -4.89793474e-01,  -1.64310264e-03,   4.26229958e-04, 3.80932152e-01]])
+#print dist
+
+def ultrasonicleft():
+    GPIO.output(TRIGGERLEFT, False)
+    time.sleep(0.5)
+    GPIO.output(TRIGGERLEFT, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIGGERLEFT, False)
+
+    while GPIO.input(ECHOLEFT) == 0:
+        start1 = time.time()
+
+    while GPIO.input(ECHOLEFT) == 1:
+        stop1 = time.time()
+
+    elapsed1 = stop1 - start1
+    left_distance = (elapsed1 * 34300) / 2
+
+
+    return left_distance
+
+def ultrasonicright():
+    GPIO.output(TRIGGERRIGHT, False)
+    time.sleep(0.5)
+    GPIO.output(TRIGGERRIGHT, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIGGERRIGHT, False)
+
+    while GPIO.input(ECHORIGHT) == 0:
+        start1 = time.time()
+
+    while GPIO.input(ECHORIGHT) == 1:
+        stop1 = time.time()
+
+    elapsed1 = stop1 - start1
+    right_distance = (elapsed1 * 34300) / 2
+
+    return right_distance
+
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
     camera.framerate = 60
@@ -15,5 +70,10 @@ with picamera.PiCamera() as camera:
             cv2.imshow('frame', hsv)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
+        left_distance = ultrasonicleft()
+        print(left_distance)
+        right_distance = ultrasonicright()
+        print(right_distance)
     cv2.destroyAllWindows()
-    print "User Stopped"
+    print "User Stopped!"
