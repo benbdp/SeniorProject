6af71a5b1d3b12447e60ea9055265ae524e8856c
Benjamin <benjamin.plamondon@gmail.com>
2017-03-01 13:12:18 -0500

    undistort and camera calibration

--- RaspberryPi/Cam.py
+++ RaspberryPi/Cam.py
@@ -1,95 +1,145 @@
+from __future__ import print_function
+import datetime
+import argparse
+import imutils
 import cv2
-import picamera
-import picamera.array
-
-import time
-import RPi.GPIO as GPIO
-import numpy as np
-import serial
-ser = serial.Serial('/dev/ttyACM0', 9600)
-
-distance_limit = 25
-#right_distance = int(input('enter right_distance: '))
-#left_distance = int(input('enter left_distance: '))
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-TRIGGERRIGHT = 18
-ECHORIGHT = 23
-GPIO.setup(TRIGGERRIGHT, GPIO.OUT)
-GPIO.setup(ECHORIGHT, GPIO.IN)
-TRIGGERLEFT = 24
-ECHOLEFT = 25
-GPIO.setup(TRIGGERLEFT, GPIO.OUT)
-GPIO.setup(ECHOLEFT, GPIO.IN)
+from threading import Thread
 
 
+class FPS:
+    def __init__(self):
+        # store the start time, end time, and total number of frames
+        # that were examined between the start and end intervals
+        self._start = None
+        self._end = None
+        self._numFrames = 0
 
-def ultrasonicleft():
-    GPIO.output(TRIGGERLEFT, False)
-    time.sleep(0.5)
-    GPIO.output(TRIGGERLEFT, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIGGERLEFT, False)
+    def start(self):
+        # start the timer
+        self._start = datetime.datetime.now()
+        return self
 
-    while GPIO.input(ECHOLEFT) == 0:
-        start1 = time.time()
+    def stop(self):
+        # stop the timer
+        self._end = datetime.datetime.now()
 
-    while GPIO.input(ECHOLEFT) == 1:
-        stop1 = time.time()
+    def update(self):
+        # increment the total number of frames examined during the
+        # start and end intervals
+        self._numFrames += 1
 
-    elapsed1 = stop1 - start1
-    left_distance = (elapsed1 * 34300) / 2
+    def elapsed(self):
+        # return the total number of seconds between the start and
+        # end interval
+        return (self._end - self._start).total_seconds()
+
+    def fps(self):
+        # compute the (approximate) frames per second
+        return self._numFrames / self.elapsed()
 
 
-    return left_distance
+# import the necessary packages
+from threading import Thread
+import cv2
 
-def ultrasonicright():
-    GPIO.output(TRIGGERRIGHT, False)
-    time.sleep(0.5)
-    GPIO.output(TRIGGERRIGHT, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIGGERRIGHT, False)
 
-    while GPIO.input(ECHORIGHT) == 0:
-        start1 = time.time()
+class WebcamVideoStream:
+    def __init__(self, src=0):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
 
-    while GPIO.input(ECHORIGHT) == 1:
-        stop1 = time.time()
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
 
-    elapsed1 = stop1 - start1
-    right_distance = (elapsed1 * 34300) / 2
+        def start(self):
+            # start the thread to read frames from the video stream
+            Thread(target=self.update, args=()).start()
+            return self
 
-    return right_distance
+        def update(self):
+            # keep looping infinitely until the thread is stopped
+            while True:
+                # if the thread indicator variable is set, stop the thread
+                if self.stopped:
+                    return
 
-with picamera.PiCamera() as camera:
-    with picamera.array.PiRGBArray(camera) as stream:
-        camera.resolution = (320, 240)
+                # otherwise, read the next frame from the stream
+                (self.grabbed, self.frame) = self.stream.read()
 
-        while True:
-            camera.capture(stream, 'bgr', use_video_port=True)
-            # stream.array now contains the image data in BGR order
-            cv2.imshow('frame', stream.array)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            # reset the stream before the next capture
-            stream.seek(0)
-            stream.truncate()
-            left_distance = ultrasonicleft()
-            print(left_distance)
-            right_distance = ultrasonicright()
-            print(right_distance)
-            if (right_distance > distance_limit) and (left_distance > distance_limit):
-                motor_speed = str(60)
-                servo_angle = str(97)
-                print motor_speed + str('m,')
-                ser.write(motor_speed + str('m,') + servo_angle + str('s,'))
+        def read(self):
+            # return the frame most recently read
+            return self.frame
 
-            else:
-                motor_speed = str(0)
-                print motor_speed + str('m,')
-                ser.write(motor_speed + str('m,'))
+        def stop(self):
+            # indicate that the thread should be stopped
+            self.stopped = True
 
-        cv2.destroyAllWindows()
-        print "User Stopped"
-        motor_speed = str(0)
-        ser.write(motor_speed + str('m,') + str(97) + str('s,'))
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+	help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+	help="Whether or not frames should be displayed")
+args = vars(ap.parse_args())
+
+# grab a pointer to the video stream and initialize the FPS counter
+print("[INFO] sampling frames from webcam...")
+stream = cv2.VideoCapture(0)
+fps = FPS().start()
+
+# loop over some frames
+while fps._numFrames < args["num_frames"]:
+    # grab the frame from the stream and resize it to have a maximum
+    # width of 400 pixels
+    (grabbed, frame) = stream.read()
+    frame = imutils.resize(frame, width=400)
+
+    # check to see if the frame should be displayed to our screen
+    if args["display"] > 0:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+    # update the FPS counter
+    fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+stream.release()
+cv2.destroyAllWindows()
+
+# created a *threaded* video stream, allow the camera sensor to warmup,
+# and start the FPS counter
+print("[INFO] sampling THREADED frames from webcam...")
+vs = WebcamVideoStream(src=0).start()
+fps = FPS().start()
+
+# loop over some frames...this time using the threaded stream
+while fps._numFrames < args["num_frames"]:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    # check to see if the frame should be displayed to our screen
+    if args["display"] > 0:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+    # update the FPS counter
+    fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
