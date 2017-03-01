2286cb0af2a0814093e1e9e776b2d28c4bb2868c
Benjamin <benjamin.plamondon@gmail.com>
2017-03-01 12:16:28 -0500

    testing cam with opencv

--- RaspberryPi/raspi_Drive.py
+++ RaspberryPi/raspi_Drive.py
@@ -77,14 +77,15 @@ with picamera.PiCamera() as camera:
 
 try:
     while True:
-        camera.capture(stream, 'bgr', use_video_port=True)
-        # stream.array now contains the image data in BGR order
-        cv2.imshow('frame', stream.array)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        # reset the stream before the next capture
-        stream.seek(0)
-        stream.truncate()
+        while True:
+            camera.capture(stream, 'bgr', use_video_port=True)
+            # stream.array now contains the image data in BGR order
+            cv2.imshow('frame', stream.array)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            # reset the stream before the next capture
+            stream.seek(0)
+            stream.truncate()
         #ser.isOpen()
         #time.sleep(0) # sampling rate
         left_distance = ultrasonicleft()
