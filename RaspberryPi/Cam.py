234f9a7ae9785f74556bb6a277b05e7b4cd0192d
Benjamin <benjamin.plamondon@gmail.com>
2017-03-01 13:36:42 -0500

    improving FPS

--- RaspberryPi/Cam.py
+++ RaspberryPi/Cam.py
@@ -66,7 +66,7 @@ def ultrasonicright():
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
-    camera.framerate = 150
+    camera.framerate = 120
     time.sleep(2) # AGC warm-up time
     while True:
         with picamera.array.PiRGBArray(camera) as stream:
