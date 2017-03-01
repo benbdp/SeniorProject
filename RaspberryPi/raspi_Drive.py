fae02f81920010c5b67d3dc366cdca35d5cf9c6f
Benjamin <benjamin.plamondon@gmail.com>
2017-03-01 11:45:12 -0500

    drive testing with ultrasonic

--- RaspberryPi/raspi_Drive.py
+++ RaspberryPi/raspi_Drive.py
@@ -76,6 +76,21 @@ def ultrasonicright():
 
 try:
     while True:
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+            image = frame.array
+
+            # show the frame
+            cv2.imshow("Frame", image)
+            key = cv2.waitKey(1) & 0xFF
+
+            # clear the stream in preparation for the next frame
+            rawCapture.truncate(0)
+
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
         #ser.isOpen()
         #time.sleep(0) # sampling rate
         left_distance = ultrasonicleft()
