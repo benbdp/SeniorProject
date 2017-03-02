5bf1d4d812eee3196647c3705ea4ae7ab959b8ef
Benjamin <benjamin.plamondon@gmail.com>
2017-03-01 21:54:40 -0500

    taking image for warp

--- RaspberryPi/Cam.py
+++ RaspberryPi/Cam.py
@@ -13,26 +13,32 @@ mtx = np.matrix([[486.7350296, 0., 319.86577798],
 dist = np.matrix([[1.72030848e-01,  -4.89793474e-01,  -1.64310264e-03,   4.26229958e-04, 3.80932152e-01]])
 #print dist
 
-
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
+camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # allow the camera to warmup
 time.sleep(0.1)
 
-# grab an image from the camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+ # grab the raw NumPy array representing the image, then initialize the timestamp
+ # and occupied/unoccupied text
+ image = frame.array
+ h, w = image.shape[:2]
+ print h, w
+ newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+ undist = cv2.undistort(image, mtx, dist, None, newcameramtx)
 
+ # show the frame
+ cv2.imshow("Frame", undist)
+ key = cv2.waitKey(1) & 0xFF
 
-h, w = image.shape[:2]
-print h,w
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-undist = cv2.undistort(image, mtx, dist, None, newcameramtx)
+ # clear the stream in preparation for the next frame
+ rawCapture.truncate(0)
 
-# display the image on screen and wait for a keypress
-cv2.imshow("Image", undist)
-cv2.waitKey(0)
-cv2.imwrite("/home/pi/Desktop/warp.jpg", undist)
+ # if the `q` key was pressed, break from the loop
+ if key == ord("q"):
+  break
