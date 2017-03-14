dd0f3c9d0251ef8a0d5d98c27ff31a76d9cc4b7e
Benjamin <benjamin.plamondon@gmail.com>
2017-03-14 19:35:30 -0400

    testing lane detection on pi

--- RaspberryPi/Serial.py
+++ RaspberryPi/Serial.py
@@ -3,12 +3,12 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 time.sleep(2)
-ser.write(str(80) + str('m,') + str(70) + str('s,'))
+ser.write(str(80) + str('m,') + str(68) + str('s,'))
 print"running!"
 time.sleep(2.75)
 ser.write(str(80) + str('m,') + str(60) + str('s,'))
 time.sleep(1)
-ser.write(str(80) + str('m,') + str(70) + str('s,'))
+ser.write(str(80) + str('m,') + str(68) + str('s,'))
 time.sleep(4)
-ser.write(str(0) + str('m,') + str(70) + str('s,'))
+ser.write(str(0) + str('m,') + str(68) + str('s,'))
 print "stopped!"
