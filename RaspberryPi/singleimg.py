6e147e2a7b3b15ac9a9e374f1ac44427c8a3c2f4
Benjamin <benjamin.plamondon@gmail.com>
2017-03-15 14:46:04 -0400

    undisortion testing

--- RaspberryPi/singleimg.py
+++ RaspberryPi/singleimg.py
@@ -1,29 +1,19 @@
 import cv2
-
+import datetime
+import time
 cap = cv2.VideoCapture(0)
-while not cap.isOpened():
-    cap = cv2.VideoCapture(0)
-    cv2.waitKey(1000)
-    print "Wait for the header"
+timestamp = datetime.datetime.now()
 
-pos_frame = cap.get(cv2.CV_CAP_PROP_POS_FRAMES)
-while True:
-    flag, frame = cap.read()
-    if flag:
-        # The frame is ready and already captured
-        cv2.imshow('video', frame)
-        pos_frame = cap.get(cv2.CV_CAP_PROP_POS_FRAMES)
-        print str(pos_frame)+" frames"
-    else:
-        # The next frame is not ready, so we try to read it again
-        cap.set(cv2.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
-        print "frame is not ready"
-        # It is better to wait for a while for the next frame to be ready
-        cv2.waitKey(1000)
+while(1):
+    timestamp = datetime.datetime.now()
+    ret, frame = cap.read()
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
 
-    if cv2.waitKey(10) == 27:
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    cv2.imshow("Frame",frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
         break
-    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_POS_FRAME_COUNT):
-        # If the number of captured frames is equal to the total number of frames,
-        # we stop
-        break
+
+cap.release()
+cv2.destroyAllWindows()
