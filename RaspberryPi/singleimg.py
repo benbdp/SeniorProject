494bdaeabb79be99cf294a1825fc5318a90f5494
Benjamin <benjamin.plamondon@gmail.com>
2017-03-15 16:12:01 -0400

    undisortion testing

--- RaspberryPi/singleimg.py
+++ RaspberryPi/singleimg.py
@@ -9,7 +9,6 @@ def camera():
     ret, frame = cap.read()
     #ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
     #cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-    cap.release()
     return frame
 
 while True:
