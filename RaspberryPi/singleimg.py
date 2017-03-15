81d8a6ca1ec7d0cc04b8654584bee09628bbdf34
Benjamin <benjamin.plamondon@gmail.com>
2017-03-15 14:24:40 -0400

    undisortion testing

--- RaspberryPi/singleimg.py
+++ RaspberryPi/singleimg.py
@@ -12,7 +12,7 @@ while(True):
 
     # Display the resulting frame
     cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey() & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
