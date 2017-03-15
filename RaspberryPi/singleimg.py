cb14247dca72d341b4f142cc27ecafccdc819df8
Benjamin <benjamin.plamondon@gmail.com>
2017-03-15 15:27:03 -0400

    undisortion testing

--- RaspberryPi/singleimg.py
+++ RaspberryPi/singleimg.py
@@ -1,19 +1,19 @@
 import cv2
 import datetime
 import time
-cap = cv2.VideoCapture(0)
-timestamp = datetime.datetime.now()
 
-while(1):
+
+def camera():
+    cap = cv2.VideoCapture(0)
+    time.sleep(1)
     timestamp = datetime.datetime.now()
     ret, frame = cap.read()
     ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-
     cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-    cv2.imshow("Frame",frame)
-    key = cv2.waitKey() & 0xFF
-    if key == ord("q"):
-        break
+    cap.release()
+    return frame
 
-cap.release()
-cv2.destroyAllWindows()
+while True:
+    frame = camera()
+    cv2.imshow("frame",frame)
+    cv2.waitKey()
