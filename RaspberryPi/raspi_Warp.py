9b1f2a63f4618fa9ecd0f9d8f9b07696cf2ba5fe
Benjamin <benjamin.plamondon@gmail.com>
2017-03-15 13:07:27 -0400

    undisortion testing

--- RaspberryPi/raspi_Warp.py
+++ RaspberryPi/raspi_Warp.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
 
-src_img = cv2.imread('/Users/Benjamin/PycharmProjects/SeniorProject/CameraCalibration/undistort.png')
+src_img = cv2.imread('/home/pi/Desktop/warp.jpg')
 cv2.imshow('src_img',src_img)
 
 print(src_img.shape)
 
-src_pts = np.float32([[343,386],[857,386],[0,558],[1256,585]])#src
+src_pts = np.float32([[72,227],[576,223],[1,316],[634,296]])#src
 
-dst_pts = np.float32([[0,0],[513,0],[0,660],[516,690]])#dst
+dst_pts = np.float32([[0,0],[556,0],[0,184],[556,156]])#dst
 
 M = cv2.getPerspectiveTransform(src_pts,dst_pts)
-dst_img = cv2.warpPerspective(src_img,M,(516,690))
+dst_img = cv2.warpPerspective(src_img,M,(556,156))
 
 cv2.imshow('dst',dst_img)
 cv2.waitKey()
