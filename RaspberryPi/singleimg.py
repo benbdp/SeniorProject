d4897d90155a93156167559be8abc54ff892c971
Benjamin <benjamin.plamondon@gmail.com>
2017-03-15 15:37:16 -0400

    undisortion testing

--- RaspberryPi/singleimg.py
+++ RaspberryPi/singleimg.py
@@ -17,20 +17,20 @@ def get_image():
     retval, im = camera.read()
     return im
 
+while 1:
+    # Ramp the camera - these frames will be discarded and are only used to allow v4l2
+    # to adjust light levels, if necessary
+    for i in xrange(ramp_frames):
+        temp = get_image()
+    print("Taking image...")
+    # Take the actual image we want to keep
+    camera_capture = get_image()
 
-# Ramp the camera - these frames will be discarded and are only used to allow v4l2
-# to adjust light levels, if necessary
-for i in xrange(ramp_frames):
-    temp = get_image()
-print("Taking image...")
-# Take the actual image we want to keep
-camera_capture = get_image()
+    # A nice feature of the imwrite method is that it will automatically choose the
+    # correct format based on the file extension you provide. Convenient!
+    cv2.imshow('camera_capture',camera_capture)
+    cv2.waitKey()
 
-# A nice feature of the imwrite method is that it will automatically choose the
-# correct format based on the file extension you provide. Convenient!
-cv2.imshow('camera_capture',camera_capture)
-cv2.waitKey()
-
-# You'll want to release the camera, otherwise you won't be able to create a new
-# capture object until your script exits
-del (camera)
+    # You'll want to release the camera, otherwise you won't be able to create a new
+    # capture object until your script exits
+    del (camera)
