30dcce9e9bbec2bc83238942a6eb66bd116f097f
Benjamin <benjamin.plamondon@gmail.com>
2017-03-15 14:31:13 -0400

    undisortion testing

--- RaspberryPi/singleimg.py
+++ RaspberryPi/singleimg.py
@@ -6,24 +6,24 @@ while not cap.isOpened():
     cv2.waitKey(1000)
     print "Wait for the header"
 
-pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+pos_frame = cap.get(cv2.CV_CAP_PROP_POS_FRAMES)
 while True:
     flag, frame = cap.read()
     if flag:
         # The frame is ready and already captured
         cv2.imshow('video', frame)
-        pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+        pos_frame = cap.get(cv2.CV_CAP_PROP_POS_FRAMES)
         print str(pos_frame)+" frames"
     else:
         # The next frame is not ready, so we try to read it again
-        cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+        cap.set(cv2.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
         print "frame is not ready"
         # It is better to wait for a while for the next frame to be ready
         cv2.waitKey(1000)
 
     if cv2.waitKey(10) == 27:
         break
-    if cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) == cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_POS_FRAME_COUNT):
         # If the number of captured frames is equal to the total number of frames,
         # we stop
         break
