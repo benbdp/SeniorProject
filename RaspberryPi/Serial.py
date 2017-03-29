667a034fce2b09cede5f8db0eb541207a27bbe71
Benjamin <benjamin.plamondon@gmail.com>
2017-03-29 10:31:46 -0400

    Testing steering

--- RaspberryPi/Serial.py
+++ RaspberryPi/Serial.py
@@ -1,14 +1,15 @@
 import serial
 import time
 ser = serial.Serial('/dev/ttyACM0', 9600)
+servo_center = 83
 
-time.sleep(2)
-ser.write(str(80) + str('m,') + str(68) + str('s,'))
-print"running!"
-time.sleep(2.75)
-ser.write(str(80) + str('m,') + str(60) + str('s,'))
 time.sleep(1)
-ser.write(str(80) + str('m,') + str(68) + str('s,'))
-time.sleep(4)
-ser.write(str(0) + str('m,') + str(68) + str('s,'))
-print "stopped!"
+
+
+while True:
+    entry = int(input("Enter change servo pos: "))
+    ser.write(str(200) + str('m,') + str(servo_center+entry) + str('s,'))
+    print"running!"
+    time.sleep(2)
+    ser.write(str(0) + str('m,') + str(servo_center) + str('s,'))
+    print "stopped!"
