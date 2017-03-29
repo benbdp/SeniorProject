71de0f38ed4dd24497e4f73a543d3df9413d9624
Benjamin <benjamin.plamondon@gmail.com>
2017-03-29 11:43:19 -0400

    Testing steering

--- RaspberryPi/singleimg.py
+++ RaspberryPi/singleimg.py
@@ -22,11 +22,12 @@ newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
 undistort = cv2.undistort(capture, mtx, dist, None, newcameramtx)
 cv2.imshow('undistort', undistort)
 cv2.imwrite('/home/pi/Desktop/warp.png',undistort)
-# src_pts = np.float32([[72, 227], [576, 223], [1, 316], [634, 296]])  # src
-# dst_pts = np.float32([[0, 0], [556, 0], [0, 184], [556, 156]])  # dst
-# M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-# dst_img = cv2.warpPerspective(undistort, M, (556, 156))
-# #cv2.imshow('warp', dst_img)
+src_pts = np.float32([[59,228],[568,227],[3,305],[625,305]])#src
+
+dst_pts = np.float32([[0,0],[558,0],[0,154],[558,154]])#dst
+
+M = cv2.getPerspectiveTransform(src_pts,dst_pts)
+dst_img = cv2.warpPerspective(undistort,M,(558,154))
 # hsv = cv2.cvtColor(dst_img, cv2.COLOR_BGR2HSV)  # Convert to HSV
 # cv2.imshow('hsv', hsv)
 # lower_blue = np.array([40, 70, 140])  # define range of color in HSV
