0a9ba13f2c8280dc9ee1728c63d1f51e8ac0bec5
Benjamin <benjamin.plamondon@gmail.com>
2017-03-29 12:13:20 -0400

    Testing steering

--- RaspberryPi/raspi_Drive.py
+++ RaspberryPi/raspi_Drive.py
@@ -7,14 +7,16 @@ import sys
 import math
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
+servo_center = 83
+distance_limit = 50
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-TRIGGERRIGHT = 18
+TRIGGERRIGHT = 24
 ECHORIGHT = 23
 GPIO.setup(TRIGGERRIGHT, GPIO.OUT)
 GPIO.setup(ECHORIGHT, GPIO.IN)
-TRIGGERLEFT = 24
-ECHOLEFT = 25
+TRIGGERLEFT = 15
+ECHOLEFT = 14
 GPIO.setup(TRIGGERLEFT, GPIO.OUT)
 GPIO.setup(ECHOLEFT, GPIO.IN)
 
@@ -57,94 +59,7 @@ def ultrasonicright():
     right_distance = (elapsed1 * 34300) / 2
 
     return right_distance
-#
-# def blur(img,kernel_size):
-#     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 1)
-# def hsv(img):
-#     return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-#
-#
-# def laneDetection(img):
-#     height, width, channels = img.shape
-#     #print height
-#     center_y = height / 2
-#     #print center_y
-#     #print width
-#     center_x = width / 2
-#     #print center_x
-#
-#     hsv = cv2.cvtColor(img), cv2.COLOR_BGR2HSV)  # Convert to HSV
-#     #cv2.imshow('hsv',hsv)
-#     lower_blue = np.array([90, 50, 190])  # define range of blue color in HSV
-#     upper_blue = np.array([120, 100, 225])
-#     mask = cv2.inRange(hsv, lower_blue, upper_blue)  # Threshold the HSV image to get only blue colors
-#     #cv2.imshow('mask',mask)
-#     dilation = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=5)
-#     erode = cv2.erode(dilation, np.ones((5, 5), np.uint8), iterations=3)
-#     #cv2.imshow('erode',erode)
-#     im2, contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#     #print contours[0]
-#
-#     newcontours = []
-#     for cnt in contours:
-#         area = cv2.contourArea(cnt)
-#         if area > 1000:
-#             newcontours.append(cnt)
-#
-#     cv2.drawContours(img, newcontours, -1, (0, 255, 0), 3)
-#     print newcontours
-#
-#
-#     rows, cols = img.shape[:2]
-#     vx0, vy0, x0, y0 = cv2.fitLine(newcontours[0], cv2.DIST_L2, 0, 0.01, 0.01)
-#     lefty0 = int((-x0 * vy0 / vx0) + y0)
-#     righty0 = int(((cols - x0) * vy0 / vx0) + y0)
-#
-#     x_00 = float(cols-1)
-#     y_00 = float(righty0)
-#     x_01 = float(0)
-#     y_01 = float(lefty0)
-#
-#     slope0 = float((y_01 - y_00)/(x_01-x_00))
-#     yint0 = y_01 - (slope0 *x_01)
-#
-#     x0 = (center_y-yint0)/slope0
-#     x0 = int(x0)
-#     cv2.circle(img, (x0, center_y), 5, (0, 0, 255), -1)
-#
-#
-#     vx1, vy1, x1, y1 = cv2.fitLine(newcontours[1], cv2.DIST_L2, 0, 0.01, 0.01)
-#     lefty1 = int((-x1 * vy1 / vx1) + y1)
-#     righty1 = int(((cols - x1) * vy1 / vx1) + y1)
-#
-#     x_10 = float(cols - 1)
-#     y_10 = float(righty1)
-#     x_11 = float(0)
-#     y_11 = float(lefty1)
-#     slope1 = float((y_11 - y_10) / (x_11 - x_10))
-#     yint1 = y_11 - (slope1 * x_11)
-#     x1 = (center_y - yint1) / slope1
-#     x1 = int(x1)
-#     cv2.circle(img, (x1, center_y), 5, (0, 0, 255), -1)
-#     center = (x0 + x1)/2
-#     cv2.circle(img, (center, center_y), 5, (0, 0, 255), -1)
-#     angle = float(math.atan2((center - center_x),center_y))
-#     angle = math.degrees(angle)
-#
-#     return angle
-#
-# mtx = np.matrix([[  1.09737118e+03, 0.00000000e+00, 6.29382303e+02], [  0.00000000e+00, 1.10083151e+03, 3.71037449e+02], [  0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
-# dist = np.matrix([[  1.37334519e-01, -1.20441566e+00, 2.19553714e-03, -4.06071434e-04, 2.18048197e+00]])
 
-
-# try:
-#     webcam = cv2.VideoCapture(0) # index of your camera
-#     time.sleep(2)
-# except:
-#     print ("problem opening input stream")
-#     sys.exit(1)
-#
-# distance_limit = 25
 try:
     while True:
         # ret, frame = webcam.read()
