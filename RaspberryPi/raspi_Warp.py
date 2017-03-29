7f33fed1bb991d9770afc3d26b046cf0f4996e00
Benjamin <benjamin.plamondon@gmail.com>
2017-03-29 11:41:16 -0400

    Testing steering

--- RaspberryPi/raspi_Warp.py
+++ RaspberryPi/raspi_Warp.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
 
-src_img = cv2.imread('/home/pi/Desktop/warp.jpg')
+src_img = cv2.imread('/home/pi/Desktop/warp.png')
 cv2.imshow('src_img',src_img)
 
 print(src_img.shape)
 
-src_pts = np.float32([[72,227],[576,223],[1,316],[634,296]])#src
+src_pts = np.float32([[59,228],[568,227],[3,305],[625,305]])#src
 
-dst_pts = np.float32([[0,0],[556,0],[0,184],[556,156]])#dst
+dst_pts = np.float32([[0,0],[558,0],[0,154],[558,154]])#dst
 
 M = cv2.getPerspectiveTransform(src_pts,dst_pts)
-dst_img = cv2.warpPerspective(src_img,M,(556,156))
+dst_img = cv2.warpPerspective(src_img,M,(558,154))
 
 cv2.imshow('dst',dst_img)
 cv2.waitKey()
