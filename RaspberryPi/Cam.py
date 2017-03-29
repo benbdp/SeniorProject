095e042a4474e5571d6feaa67302f7d051e5e58b
Benjamin <benjamin.plamondon@gmail.com>
2017-03-29 15:28:29 -0400

    testing lane detection

--- RaspberryPi/Cam.py
+++ RaspberryPi/Cam.py
@@ -85,62 +85,62 @@ def get_image():
 
 for i in xrange(junk_frames):
     temp = get_image()
+def lane_detection(img):
+    #cv2.imshow('frame', frame)
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+    undistort = cv2.undistort(img, mtx, dist, None, newcameramtx)
+    cv2.imshow('undistort', undistort)
+    src_pts = np.float32([[59,228],[568,227],[3,305],[625,305]])#src
 
-capture = temp
-#cv2.imshow('frame', frame)
-h, w = capture.shape[:2]
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-undistort = cv2.undistort(capture, mtx, dist, None, newcameramtx)
-cv2.imshow('undistort', undistort)
-src_pts = np.float32([[59,228],[568,227],[3,305],[625,305]])#src
+    dst_pts = np.float32([[0,0],[558,0],[0,154],[558,154]])#dst
 
-dst_pts = np.float32([[0,0],[558,0],[0,154],[558,154]])#dst
+    M = cv2.getPerspectiveTransform(src_pts,dst_pts)
+    dst_img = cv2.warpPerspective(undistort,M,(558,154))
+    cv2.imshow('warp', dst_img)
+    hsv = cv2.cvtColor(dst_img, cv2.COLOR_BGR2HSV)  # Convert to HSV
+    cv2.imshow('hsv', hsv)
+    lower_blue = np.array([40, 70, 140])  # define range of color in HSV
+    upper_blue = np.array([60,255,255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)  # Threshold the HSV image to get only desired color
+    cv2.imshow('mask', mask)
+    height, width, channels = dst_img.shape
+    # print height
+    center_y = height / 2
+    # print center_y
+    # print width
+    center_x = width / 2
+    # print center_x
+    dilation = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=6)
+    erode = cv2.erode(dilation, np.ones((5, 5), np.uint8), iterations=6)
+    cv2.imshow('erode',erode)
+    im2, contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # print contours[0]
 
-M = cv2.getPerspectiveTransform(src_pts,dst_pts)
-dst_img = cv2.warpPerspective(undistort,M,(558,154))
-cv2.imshow('warp', dst_img)
-hsv = cv2.cvtColor(dst_img, cv2.COLOR_BGR2HSV)  # Convert to HSV
-cv2.imshow('hsv', hsv)
-lower_blue = np.array([40, 70, 140])  # define range of color in HSV
-upper_blue = np.array([60,255,255])
-mask = cv2.inRange(hsv, lower_blue, upper_blue)  # Threshold the HSV image to get only desired color
-cv2.imshow('mask', mask)
-height, width, channels = dst_img.shape
-# print height
-center_y = height / 2
-# print center_y
-# print width
-center_x = width / 2
-# print center_x
-dilation = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=6)
-erode = cv2.erode(dilation, np.ones((5, 5), np.uint8), iterations=6)
-cv2.imshow('erode',erode)
-im2, contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# print contours[0]
+    newcontours = []
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            newcontours.append(cnt)
+    cv2.drawContours(dst_img, newcontours, -1, (0, 255, 0), 3)
+    #print newcontours
 
-newcontours = []
-for cnt in contours:
-    area = cv2.contourArea(cnt)
-    if area > 100:
-        newcontours.append(cnt)
-cv2.drawContours(dst_img, newcontours, -1, (0, 255, 0), 3)
-#print newcontours
+    num_contours = len(newcontours)
+    if num_contours == 2 :
+        print "Found two lines"
+        zero = line(erode,newcontours[0],center_y)
+        one = line(erode,newcontours[1],center_y)
 
-num_contours = len(newcontours)
-if num_contours == 2 :
-    print "Found two lines"
-    zero = line(erode,newcontours[0],center_y)
-    one = line(erode,newcontours[1],center_y)
+        centerlane = (zero + one) / 2
+        error = center_x - centerlane
+        print error
+        return error
 
-    centerlane = (zero + one) / 2
-    error = center_x - centerlane
-    print error
+    elif num_contours == 1:
+        print "Found one line"
+    else:
+        print "error"
 
-elif num_contours == 1:
-    print "Found one line"
-else:
-    print "error"
-
-
+lane_detection(temp)
 cv2.waitKey()
 cv2.destroyAllWindows()
