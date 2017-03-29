96b3fa5ead5523dc2e79ac52a1ab61e0214cc824
Benjamin <benjamin.plamondon@gmail.com>
2017-03-29 10:47:41 -0400

    Testing steering

--- RaspberryPi/Serial.py
+++ RaspberryPi/Serial.py
@@ -8,13 +8,13 @@ time.sleep(1)
 
 while True:
     entry = int(input("Enter change servo pos: "))
-    ser.write(str(200) + str('m,') + str(servo_center+entry) + str('s,'))
+    ser.write(str(100) + str('m,') + str(servo_center+entry) + str('s,'))
     print"running!"
     time.sleep(1)
     ser.write(str(0) + str('m,') + str(servo_center) + str('s,'))
     time.sleep(1)
     print "reversing!"
-    ser.write(str(-50) + str('m,') + str(servo_center+entry) + str('s,'))
+    ser.write(str(-100) + str('m,') + str(servo_center+entry) + str('s,'))
     time.sleep(1)
     ser.write(str(0) + str('m,') + str(servo_center) + str('s,'))
     print "stopped!"
