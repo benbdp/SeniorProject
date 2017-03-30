ed1bab488f8f91b76933b8f8bbd74288bac3cb9d
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-03-29 23:15:01 -0400

    Lane detection

--- RaspberryPi/singleimg.py
+++ RaspberryPi/singleimg.py
@@ -32,8 +32,8 @@ cv2.imshow("warp",dst_img)
 hsv = cv2.cvtColor(dst_img, cv2.COLOR_BGR2HSV)  # Convert to HSV
 cv2.imshow('hsv', hsv)
 cv2.imwrite("/home/pi/Desktop/hsv.png",hsv)
-lower_blue = np.array([40, 70, 140])  # define range of color in HSV
-upper_blue = np.array([200,255,255])
+lower_blue = np.array([50, 50, 130])  # define range of color in HSV
+upper_blue = np.array([90,90,170])
 mask = cv2.inRange(hsv, lower_blue, upper_blue)  # Threshold the HSV image to get only desired color
 cv2.imshow('mask', mask)
 cv2.waitKey()
