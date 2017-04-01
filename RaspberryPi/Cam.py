2eba19f751ee938039c72d8efe31b7dc5317fc47
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-01 18:58:05 -0400

    Lane detection

--- RaspberryPi/Cam.py
+++ RaspberryPi/Cam.py
@@ -82,15 +82,15 @@ def get_image():
     return img
 
 def forward():
-    ser.write(str(100) + str('m,') + str(servo_center) + str('s,'))
+    ser.write(str(60) + str('m,') + str(servo_center) + str('s,'))
 def stop():
     ser.write(str(0) + str('m,') + str(servo_center) + str('s,'))
 
 def left():
-    ser.write(str(100) + str('m,') + str(servo_center-10) + str('s,'))
+    ser.write(str(60) + str('m,') + str(servo_center-10) + str('s,'))
 
 def right():
-    ser.write(str(100) + str('m,') + str(servo_center+10) + str('s,'))
+    ser.write(str(60) + str('m,') + str(servo_center+10) + str('s,'))
 
 def contours(img): # img should be wrapped image
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # Convert to HSV
