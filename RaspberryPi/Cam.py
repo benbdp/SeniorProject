7a63da2213a3a80816b6de593053fd2c1b53cdf6
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-03 16:59:52 -0400

    Optimization

--- RaspberryPi/Cam.py
+++ RaspberryPi/Cam.py
@@ -152,7 +152,7 @@ try:
         right_distance = ultrasonicright()
         #print right_distance
         if (right_distance > distance_limit) and (left_distance > distance_limit):
-            lane_detection(frame(10))
+            lane_detection(frame(1))
         else:
             stop()
 except:
