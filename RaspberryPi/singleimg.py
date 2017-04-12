7e0a68b3aa20a944aad7977a7fd1582044d9cd04
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-04-12 18:41:13 -0400

    commenting code

--- RaspberryPi/singleimg.py
+++ RaspberryPi/singleimg.py
@@ -1,39 +1,39 @@
 import cv2
 import numpy as np
 
-# mtx = np.load('/home/pi/Cal_Imgs/cameramatrix.npy')
-# dist = np.load('/home/pi/Cal_Imgs/distortioncoeff.npy')
-#
-# junk_frames = 30
-# camera = cv2.VideoCapture(0)
-#
-# def get_image():
-#     retval, img = camera.read()
-#     return img
-#
-#
-# for i in xrange(junk_frames):
-#     temp = get_image()
-#
-# capture = get_image()
-# cv2.imwrite('/home/pi/Desktop/imgs/frame.png',capture)
-#
-# #cv2.imshow('frame', frame)
-# h, w = capture.shape[:2]
-# newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-# undistort = cv2.undistort(capture, mtx, dist, None, newcameramtx)
-# #cv2.imshow('undistort', undistort)
-# cv2.imwrite('/home/pi/Desktop/imgs/undistort.png',undistort)
-# src_pts = np.float32([[59,228],[568,227],[3,305],[625,305]])#src
-#
-# dst_pts = np.float32([[0,0],[558,0],[0,154],[558,154]])#dst
-#
-# M = cv2.getPerspectiveTransform(src_pts,dst_pts)
-# dst_img = cv2.warpPerspective(undistort,M,(558,154))
-# cv2.imwrite('/home/pi/Desktop/imgs/warped.png',dst_img)
-# #cv2.imshow("warp",dst_img)
-# hsv = cv2.cvtColor(dst_img, cv2.COLOR_BGR2HSV)  # Convert to HSV
-# cv2.imwrite('/home/pi/Desktop/imgs/hsv.png', hsv)
+mtx = np.load('/home/pi/Cal_Imgs/cameramatrix.npy')
+dist = np.load('/home/pi/Cal_Imgs/distortioncoeff.npy')
+
+junk_frames = 30
+camera = cv2.VideoCapture(0)
+
+def get_image():
+    retval, img = camera.read()
+    return img
+
+
+for i in xrange(junk_frames):
+    temp = get_image()
+
+capture = get_image()
+cv2.imwrite('/home/pi/Desktop/imgs/frame.png',capture)
+
+#cv2.imshow('frame', frame)
+h, w = capture.shape[:2]
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+undistort = cv2.undistort(capture, mtx, dist, None, newcameramtx)
+#cv2.imshow('undistort', undistort)
+cv2.imwrite('/home/pi/Desktop/imgs/undistort.png',undistort)
+src_pts = np.float32([[59,228],[568,227],[3,305],[625,305]])#src
+
+dst_pts = np.float32([[0,0],[558,0],[0,154],[558,154]])#dst
+
+M = cv2.getPerspectiveTransform(src_pts,dst_pts)
+dst_img = cv2.warpPerspective(undistort,M,(558,154))
+cv2.imwrite('/home/pi/Desktop/imgs/warped.png',dst_img)
+#cv2.imshow("warp",dst_img)
+hsv = cv2.cvtColor(dst_img, cv2.COLOR_BGR2HSV)  # Convert to HSV
+cv2.imwrite('/home/pi/Desktop/imgs/hsv.png', hsv)
 # #cv2.imshow('hsv', hsv)
 # lower_blue = np.array([50, 50, 130])  # define range of color in HSV
 # upper_blue = np.array([95, 140, 220])
