6e2225a6eb2d25d24d97fd67630f24746a4e485f
root <ben11.bdp@gmail.com>
2017-05-02 18:11:14 -0400

    find slope of lines

--- RaspberryPi/tunehsv.py
+++ RaspberryPi/tunehsv.py
@@ -97,7 +97,7 @@ while True:
             y_1 = float(lefty)
 
             slope = float((y_1 - y_0) / (x_1 - x_0))
-            print("slope%d: " %num)
+            print("slope%d: " %num,slope)
 
             # Finally draw the line
             cv2.line(warp, (warp.shape[1] - 1, righty), (0, lefty), 255, 2)
