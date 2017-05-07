4a9aa80dab7d4b3707ae836aec478266eaa3f0af
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-07 08:31:27 -0400

    new drive file

--- RaspberryPi/Testing/prom.py
+++ RaspberryPi/Testing/prom.py
@@ -5,24 +5,24 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 servo_center = 82  # servo position to drive straight
 
 def forward():
-    ser.write(str(63) + str('m,') + str(servo_center) + str('s,'))
+    ser.write(str(80) + str('m,') + str(servo_center) + str('s,'))
 # function that will stop the car
 def stop():
     ser.write(str(0) + str('m,') + str(servo_center) + str('s,'))
 # function to turn left
 def left(angle):
-    ser.write(str(63) + str('m,') + str(servo_center-angle) + str('s,'))
+    ser.write(str(80) + str('m,') + str(servo_center-angle) + str('s,'))
 # function to turn right
 def right(angle):
-    ser.write(str(63) + str('m,') + str(servo_center+angle) + str('s,'))
+    ser.write(str(80) + str('m,') + str(servo_center+angle) + str('s,'))
 
 def p():
     forward()
     time.sleep(1)
     right(10)
     time.sleep(0.5)
-    forward()
-    time.sleep(0.5)
+    # forward()
+    # time.sleep(0.5)
 
 
 def r():
@@ -53,3 +53,4 @@ def main():
 
 if __name__ == "__main__":
     p()
+    stop()
