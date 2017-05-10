b8c4485463d4fdca0ce5d9bd577a391e9046206b
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-09 22:09:34 -0400

    prepping code for presentation

--- RaspberryPi/Testing/Manual.py
+++ RaspberryPi/Testing/Manual.py
@@ -5,7 +5,7 @@ import sys
 import Tkinter as tk
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-servo_center = 92
+servo_center = 95
 distance_limit = 50
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
