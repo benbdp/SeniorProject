f971c25dc82e81a8d1b9788f3beb5b7185b851d1
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-09 22:49:38 -0400

    prepping code for presentation

--- RaspberryPi/Testing/raspi_Warp.py
+++ RaspberryPi/Testing/raspi_Warp.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-mtx = np.load('/home/pi/Cal_Imgs/cameramatrix.npy')
-dist = np.load('/home/pi/Cal_Imgs/distortioncoeff.npy')
+mtx = np.load('/home/pi/SeniorProject/RaspberryPi/cameramatrix.npy')
+dist = np.load('/home/pi/SeniorProject/RaspberryPi/distortioncoeff.npy')
 
 junk_frames = 30
 camera = cv2.VideoCapture(0)
@@ -23,12 +23,12 @@ undistort = cv2.undistort(capture, mtx, dist, None, newcameramtx)
 src_img = undistort
 cv2.imshow('src_img',src_img)
 
-src_pts = np.float32([[59,228],[568,227],[3,305],[625,305]])#src
+src_pts = np.float32([[87,176],[552,177],[4,301],[638,302]])#src
 
-dst_pts = np.float32([[0,0],[558,0],[0,154],[558,154]])#dst
+dst_pts = np.float32([[0,0],[558,0],[0,264],[558,264]])#dst
 
 M = cv2.getPerspectiveTransform(src_pts,dst_pts)
-dst_img = cv2.warpPerspective(src_img,M,(558,154))
+dst_img = cv2.warpPerspective(src_img,M,(558,264))
 
 cv2.imshow('dst',dst_img)
 cv2.waitKey()
