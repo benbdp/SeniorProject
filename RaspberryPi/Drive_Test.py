890c31d4439ff4afd775c55576c6cf3c9600137e
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-10 01:47:18 -0400

    testing drive

--- RaspberryPi/Drive_Test.py
+++ RaspberryPi/Drive_Test.py
@@ -169,11 +169,11 @@ def lanedetection(img,lower):
             # right()
             # print "right"
 
-            return "left"
+            return "right"
         if m > 0:  # result if line is right of image
             # left()
             # print "left"
-            return "right"
+            return "left"
 
     else:  # result if no lines or too many lines
         # stop()
