8615900c106372bfea67cffe76252d059ba7c883
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-09 23:50:29 -0400

    testing drive

--- RaspberryPi/Drive_Test.py
+++ RaspberryPi/Drive_Test.py
@@ -67,10 +67,6 @@ def ultrasonicright():
 
     return right_distance
 
-# function to retrieve image from camera
-def get_image():
-    retval, img = camera.read()
-    return img
 
 # function to drive forward
 def forward():
@@ -89,6 +85,11 @@ def right():
     ser.write(str(63) + str('m,') + str(servo_center+7) + str('s,'))
     print 'right'
 
+def center(contour):
+    M = cv2.moments(contour)
+    cx = int(M['m10'] / M['m00'])  # return x coordinate of center contour
+    return cx
+
 def line(img,contours):
     rows, cols = img.shape[:2]
     vx0, vy0, x0, y0 = cv2.fitLine(contours, cv2.DIST_L2, 0, 0.01, 0.01)
@@ -101,79 +102,86 @@ def line(img,contours):
     slope0 = float((y_01 - y_00) / (x_01 - x_00))
     return slope0
 
-def contours(img,lower): # img should be wrapped image
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # Convert to HSV
-    upper = np.array([180,255,255])# define range of color in HSV
+
+def lanedetection(img,lower):
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+    undistort = cv2.undistort(img, mtx, dist, None, newcameramtx)  # undistort image
+    src_pts = np.float32([[87, 176], [552, 177], [4, 301], [638, 302]])  # source points
+    dst_pts = np.float32([[0, 0], [558, 0], [0, 264], [558, 264]])  # destination points
+    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    warp = cv2.warpPerspective(undistort, M, (558, 264))  # warp the image
+    hsv = cv2.cvtColor(warp, cv2.COLOR_BGR2HSV)  # Convert to HSV
+    upper = np.array([180, 255, 255])  # define range of color in HSV
     mask = cv2.inRange(hsv, lower, upper)  # Threshold the HSV image to get only desired color
     h, w = mask.shape[:2]
     crop_img = mask[200:h, 0:w]
     dilation = cv2.dilate(crop_img, np.ones((5, 5), np.uint8), iterations=2)  # dilate pixels to fill in gaps
     erode = cv2.erode(dilation, np.ones((5, 5), np.uint8), iterations=1)  # cut away border pixels to reduce size
     im2, contours, hierarchy = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # contour detection
-    return contours
-
-def center(contour):
-    M = cv2.moments(contour)
-    cx = int(M['m10'] / M['m00'])  # return x coordinate of center contour
-    return cx
-
-def lane_detection(img):
-    h, w = img.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-    undistort = cv2.undistort(img, mtx, dist, None, newcameramtx)  # undistort image
-    src_pts = np.float32([[87,176],[552,177],[4,301],[638,302]])  # source points
-    dst_pts = np.float32([[0,0],[558,0],[0,264],[558,264]])  # destination points
-    M = cv2.getPerspectiveTransform(src_pts,dst_pts)
-    warp = cv2.warpPerspective(undistort,M,(558,264)) # warp the image
-    cont = contours(warp,lower)
-    height, width, channels = warp.shape
-    center_x = width / 2
     newcontours = []
-    for cnt in cont:
+    for cnt in contours:
         area = cv2.contourArea(cnt)
         print "area", area
         if area > 400:  # run test to ensure small contours are eliminated
             newcontours.append(cnt)
+
     num_contours = len(newcontours)
+
     if num_contours == 2:  # result if two lines
+        height, width, channels = warp.shape
+        center_x = width / 2
         center0 = center(newcontours[0])
         print "center0: ", center0
         center1 = center(newcontours[1])
         print "center1: ", center1
-
-        center_car = (center0+center1)/2
+        center_car = (center0 + center1) / 2
 
         # calculate how far off center the car is.
-        error = (center_x-center_car) - 5 # - 5 = fudge factor ...
+        error = (center_x - center_car) - 5  # - 5 = fudge factor ...
 
-        print "error",error
-
+        print "error", error
 
         if error > 12:
             # print "left"
             left()
+            return "left"
 
         elif error < -12:
             # print "right"
             right()
+            return "right"
 
         else:
             # print " forward"
             forward()
+            return "forward"
 
 
     elif num_contours == 1:  # result if one lane lines
-        m = line(img,newcontours[0])
+        m = line(img, newcontours[0])
 
         if m < 0:  # result if line is left of image
             right()
             # print "right"
+
+            return "right"
         if m > 0:  # result if line is right of image
             left()
             # print "left"
+            return "left"
 
     else:  # result if no lines or too many lines
         stop()
+        return "stop"
+
+
+def get_frame():
+    retval, img = camera.read()
+    return img
+
+
+
 
 
 
@@ -185,11 +193,10 @@ def main():
             print "left dist: ", left_distance
             right_distance = ultrasonicright()
             print "right dist: ", right_distance
-            if (right_distance > distance_limit) and (
-                left_distance > distance_limit):  # if car is safe distance from object drive!
-                lane_detection(frame(8))
-            else:
-                stop()  # if the car is not a safe distance do not move
+            if (right_distance > distance_limit) and (left_distance > distance_limit):  # if car is safe distance from object drive!
+                frame = get_frame()
+                what_to_do = lanedetection(frame,lower)
+                print what_to_do
 
     except:
         stop()  # stop car when program is stopped
