f6d420acfc0d2827c938a6ab7d5cebaecc579541
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-11 23:48:47 -0400

    testing drive

--- RaspberryPi/Drive.py
+++ RaspberryPi/Drive.py
@@ -185,7 +185,7 @@ def main():
             print "right dist: ", right_distance
             if (right_distance > distance_limit) and (
                 left_distance > distance_limit):  # if car is safe distance from object drive!
-                lane_detection(frame(6))
+                lane_detection(frame(4))
             else:
                 stop()  # if the car is not a safe distance do not move
 
