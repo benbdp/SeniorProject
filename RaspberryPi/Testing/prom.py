28e88ae7f16cdfe07550db505879f294801175a8
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-14 20:52:58 -0400

    Clean up. Code for Presentation

--- RaspberryPi/Testing/prom.py
+++ RaspberryPi/Testing/prom.py
@@ -4,56 +4,6 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 
 servo_center = 95  # servo position to drive straight
 
-def forward():
-    done = False
-    while not done:
-        ser.write(str(200) + str('m,') + str(servo_center) + str('s,'))
-        time.sleep(2)
-        done = True
-
-# function that will stop the car
-def stop():
-    ser.write(str(0) + str('m,') + str(servo_center) + str('s,'))
-# function to turn left
-def left(angle):
-    ser.write(str(100) + str('m,') + str(servo_center-angle) + str('s,'))
-# function to turn right
-def right(angle):
-    ser.write(str(100) + str('m,') + str(servo_center+angle) + str('s,'))
-
-def p():
-    done = False
-    while not done:
-        ser.write(str(200) + str('m,'))
-        time.sleep(2)
-        done = True
-    stop()
-
-# def r():
-#     forward()
-#     time.sleep(1)
-#     right(10)
-#
-# def o():
-#     forward()
-#     time.sleep(1)
-#     right(12)
-#     time.sleep(3)
-#
-# def m():
-#     forward()
-#     right()
-#     left()
-#     right()
-#     forward()
-
-def main():
-    p()
-    # r()
-    # o()
-    # m()
-    # stop()
-
-
-if __name__ == "__main__":
-    p()
+ser.write(str(100) + str('m,') + str(servo_center) + str('s,'))
+time.sleep(5)
+ser.write(str(0) + str('m,') + str(servo_center) + str('s,'))
