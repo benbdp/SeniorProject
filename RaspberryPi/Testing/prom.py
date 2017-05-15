c54fba4c542906e49cacfee75f61b145335bc09d
Benjamin Plamondon <benjamin.plamondon@gmail.com>
2017-05-14 20:54:06 -0400

    Clean up. Code for Presentation

--- RaspberryPi/Testing/prom.py
+++ RaspberryPi/Testing/prom.py
@@ -4,6 +4,4 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 
 servo_center = 95  # servo position to drive straight
 
-ser.write(str(100) + str('m,') + str(servo_center) + str('s,'))
-time.sleep(5)
-ser.write(str(0) + str('m,') + str(servo_center) + str('s,'))
+ser.write(str(100) + str('m,'))
