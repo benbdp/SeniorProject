ca79907e00697a444b7695d31407f06c99856937
Benjamin <benjamin.plamondon@gmail.com>
2017-11-07 15:14:12 -0500

    cleaning up structure

--- CameraCalibration/Error.py
+++
@@ -1,7 +0,0 @@
-mean_error = 0
-for i in xrange(len(objpoints)):
-    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-    tot_error += error
-
-print ("total error: ", mean_error/len(objpoints))
